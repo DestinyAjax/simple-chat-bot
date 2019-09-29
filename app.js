f658052991b796cd943606bc4711b9a9ae91044a
Destiny Ajakaiye <destinyajakaiye@gmail.com>
2019-09-30 00:42:38 +0100

    Update chat codes

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var restify = require('restify');
-var builder = require('botbuilder');
+const restify = require('restify');
+const botbuilder = require('botbuilder');
 
 /**
  * We should create a connector that will be used to interact with the bot. 
  * In the following code, we are creating a console connector:
  */
-var connector = new builder.ConsoleConnector().listen();
+// var connector = new builder.ConsoleConnector().listen();
 
 /**
  * The logic to manage the bots lies within the UniversalBot class , which accepts a variety of connectors. 
  * In the following code, we pass the console connector just created to the UniversalBot:
  */
-var bot = new builder.UniversalBot(connector);
+// var bot = new builder.UniversalBot(connector);
 
 /**
  * Bot conversations can be done in multiple styles, one of which is dialogs. 
@@ -19,4 +19,31 @@ var bot = new builder.UniversalBot(connector);
  * by the application. Dialogs follow the same pattern; 
  * each dialog can be thought of as a route within the conversational approach. 
  */
-
+ 
+// Create bot adapter, which defines how the bot sends and receives messages.
+var adapter = new botbuilder.BotFrameworkAdapter({
+    appId: process.env.MICROSOFT_BOT_APP_ID,
+    appPassword: process.env.MICROSOFT_BOT_APP_PASSWORD
+});
+ 
+// Create HTTP server.
+let server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log(`\n${server.name} listening to ${server.url}`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+});
+ 
+// Listen for incoming requests at /api/messages.
+server.post('/api/messages', (req, res) => {
+    // Use the adapter to process the incoming web request into a TurnContext object.
+    adapter.processActivity(req, res, async (turnContext) => {
+        // Do something with this incoming activity!
+        if (turnContext.activity.type === 'message') {            
+            // Get the user's text
+            const utterance = turnContext.activity.text;
+ 
+            // send a reply
+            await turnContext.sendActivity(`I heard you say ${ utterance }`);
+        }
+    });
+});
